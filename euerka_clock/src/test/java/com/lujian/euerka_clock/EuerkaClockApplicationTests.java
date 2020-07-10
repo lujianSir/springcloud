f791d47ac96e3933f3365a0778d1cfaa1e505b45
Administrator <1972689216@qq.com>
2020-07-10 10:16:18 +0800

    添加打卡

---
+++ euerka_clock/src/test/java/com/lujian/euerka_clock/EuerkaClockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lujian.euerka_clock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EuerkaClockApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
